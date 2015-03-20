4a57afba512902cc7fdce38e0393a48d57912ff7
bmagnantb <bmagnantb@gmail.com>
2015-03-20 12:56:27 -0500

    server test

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 function startServer() {
     var express = require('express'),
-        http = require('http'),
-        path = require('path'),
         app = express();
 
-    app.set('port', process.argv[3] || process.env.PORT || 3000);
-    app.use(express.static(path.join(__dirname, '')));
 
-    http.createServer(app).listen(app.get('port'), function() {
-        console.log('Express server listening on port ' + app.get('port'));
-    });
+
+}
+
+function startServer() {
+		var express = require('express'),
+				app = express()
+
+		app.use(express.static(__dirname + '/public'))
+
+		app.listen(3000)
+		console.log('skylines w/ server')
 }
 
 startServer();
